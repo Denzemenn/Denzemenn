e9c0423a79573100eb04b48cf8072552f36d2251
movavi_school <selleran@list.ru>
2022-11-27 13:13:38 +0700

    27 nov

--- lesson_10/caesar.py
+++ lesson_10/caesar.py
@@ -1,6 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-print(len(alphabet))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+symbols = ['(', ')', ', ', ' ', '.', '!', '?']
 
 should_end = False
 while not should_end:
@@ -13,44 +13,18 @@ while not should_end:
     elif shift < -len(alphabet):
         shift = shift % -len(alphabet)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    cipher_text = ''
+    for letter in text:
+        if letter in symbols:
+            cipher_text += letter
+        else:
+            position = alphabet.index(letter)
+            if position + shift > len(alphabet):
+                new_position = position + shift - len(alphabet)
+            elif position + shift < 0:
+                new_position = position + shift + len(alphabet)
+            else:
+                new_position = position + shift
+            cipher_text += alphabet[new_position]
+    print(cipher_text)
 
