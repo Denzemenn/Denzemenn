d7111334d8b800b446a1827996c2d10a012914fc
movavi_school <lavr.@yandex.ru>
2022-12-18 13:15:37 +0700

    18 dec 2022

---
+++ lesson_13/lesson_13.py
@@ -0,0 +1,53 @@
+# # empty_dictionary = {}
+# # empty_dictionary1 = dict()
+# # dict1 = {"key1": 1,
+# #          "key2": "балдёжь",
+# #          "key3": [1, 2, 3],
+# #          "key4": {"вл1": True}}
+# # print(dict1["key2"])
+# #
+# 
+# # set1 = {"anton", "anton", "anton"}
+# # print(set1)
+# # set2 = {"a", "b", "z"}
+# # print(set2)
+# # set3 = {1, "one", [1] }
+# #
+# 
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6, 7}
+# print(set1.union(set2)) # или |
+# 
+# print(set1.intersection(set2)) # или &
+# 
+# print(set1.difference(set2)) # или -
+# 
+# print(set1.symmetric_difference(set2)) # или ^
+
+from random import randint as rint
+# lst = []
+# for i in range(5):
+#     lst.append(r(1, 5))
+# print(lst)
+#
+# unique = set(lst)
+#
+# print(f"{len(unique)}шт.: {unique}")
+
+size = rint(100, 1000)
+r_start = 0
+r_end = 10_000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
