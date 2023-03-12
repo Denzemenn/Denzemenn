49059328b6ffa80e80815e9a401fa4adb3c11068
smir-ant <abcsmir.ant@gmail.com>
2023-03-12 13:10:54 +0700

    classes

---
+++ lesson_23/main.py
@@ -0,0 +1,64 @@
+# import json
+# a = open("file.json", "w", encoding="utf-8")
+# con = json.dump("Hello", a, ensure_ascii=False)
+# a = open("file.json", "r", encoding="utf-8")
+# con = json.load()
+# print(con)
+# a.close()
+
+# def common_method():
+#     print("fuck this shit")
+#
+#
+# class Person:
+#     def _init_(self, imya):
+#         self.mishurik = imya
+#         self.status = "Препод"
+#
+#     def _str_(self):
+#         return f"что: {self.mishurik} {self.status}"
+#
+#
+# # =================================================
+#
+# chelovek = Person()
+# print(chelovek.mishurik)
+# print(chelovek)
+# print(common_method())
+
+
+
+
+
+
+# class chuvak:
+#     def __init__(self, name, fam, age):
+#         self.name = name
+#         self.fam = fam
+#         self.age = age
+#     def __str__(self):
+#         return f"Имя: {self.name},\nФамилия: {self.fam},\nВозраст: {self.age}"
+#     def greet(self):
+#         return f"Привет! Меня зовут {self.name} {self.fam}, мне {self.age} лет."
+# smt = chuvak("Денис", "Черенков", 12)
+#
+# print(smt.greet())
+
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
